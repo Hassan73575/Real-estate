878110cb8ead01e1bfd7d1b1a9eeaf8e44435570
Hassan73575 <hanjum22528@gmail.com>
2025-08-20 04:55:18 +0500

    92%

--- rentshop.js
+++ rentshop.js
@@ -4,27 +4,9 @@ async function linkwaladata() {
     for (let keys in data.properties) {
       document.getElementById("print").innerHTML += `
         <div class="card">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="img/open-house-1163353_1920.jpg" class="d-block w-100" alt="...">
+                    <div class="map">
+                    ${data.properties[keys].location.map_embed}
                 </div>
-                <div class="carousel-item">
-                <img src="" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-            </div>
 
                 <i class="fas fa-home card-icon"></i>
                 <h2>${data.properties[keys].title}</h2>
